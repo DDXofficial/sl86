344b652903a85e0d74f1ba93db74054489901618
Segev A. (DDX) <ddxofficial@outlook.com>
2025-05-03 08:46:28 -0400

    Yet Another Update of Colossal Proportions
    Added a main menu, sl86 header stays at the top, etc.

--- launcher.py
+++ launcher.py
@@ -15,15 +15,18 @@ def clear_screen():
 clear_screen()
 
 # VERSION
-launcher_version = str("0.4")
+launcher_version = str("0.5")
 
 # INTRO OUTPUT
-print("         ______  _____")
-print("   _____/ ( __ )/ ___/  snakelauncher86 (sl86) - Version", launcher_version)
-print("  / ___/ / __  / __ \ 	A text-mode 86Box machine manager written in Python")
-print(" (__  ) / /_/ / /_/ / 	Author: Segev A. (DDX) - ddxofficial@outlook.com")
-print("/____/_/\____/\____/  	Source code: https://github.com/ddxofficial/sl86")
-print("")
+def sl86_header():
+    print("         ______  _____")
+    print("   _____/ ( __ )/ ___/  snakelauncher86 (sl86) - Version", launcher_version)
+    print("  / ___/ / __  / __ \ 	A text-mode 86Box machine manager written in Python")
+    print(" (__  ) / /_/ / /_/ / 	Author: Segev A. (DDX) - ddxofficial@outlook.com")
+    print("/____/_/\____/\____/  	Source code: https://github.com/DDXofficial/sl86")
+    print("")
+
+sl86_header()
 
 # ERROR HANDLING (EXIT)
 def error_quit_text():
@@ -106,12 +109,12 @@ for file in machine_dir:
     if os.path.isdir(machines_list):
         machines.append(file)
 
-# JUST SO I DON'T COPY HUGE CHUNKS OF CODE EVERY TIME:
+# INFORMATIONAL BLOCKS OF TEXT
 def quit_text():
     print("Thank you for using sl86!")
     print("")
     print("snakelauncher86 (sl86) - Version", launcher_version)
-    print("https://github.com/ddxofficial/sl86")
+    print("https://github.com/DDXofficial/sl86")
 
 def path_86box_text():
     print("86Box path:")
@@ -127,71 +130,107 @@ def info_86box():
     subprocess.run([app_path, "-Y"])
     print("")
 
-# VIEW APP AND MACHINE PATHS
-path_86box_text()
-print("")
-path_machine_text()
-info_86box()
+def machine_create_instructions():
+    print("* HOW TO CREATE A MACHINE *")
+    print("")
+    print("Create a folder inside the designated machines folder and relaunch")
+    print("the script.")
+    print("Any machines/folders created while this script is active will not be")
+    print("detected and you will need to relaunch the script for them to appear.")
+    print("This will be worked on in future releases.")
+    print("")
 
-# INSTRUCTIONS (i STILL suck at python rn leave me alone)
-print("To create a machine, create a folder inside the designated 'machines' folder")
-print("and relaunch the script.\n")
-print("NOTE: Any machines/folders created while this script is active will not be")
-print("detected and you will need to relaunch the script for them to appear.")
-print("This will be worked on in future releases.")
-print("")
+def sl86_main_menu():
+    print("* MAIN MENU *")
+    print("")
+    print("1 - Select machine")
+    print("2 - Display 86Box information")
+    print("0 - Exit")
+    print("")
 
 while True:
-    # MACHINE LISTING
-    machines.sort(key=str.lower)
-    print("Machines detected in directory:", machines_count, "\n")
-    for i in range(machines_count):
-        print(i + 1, "-", machines[i])
-    print("")
+    # MAIN MENU
+    machine_create_instructions()
+    sl86_main_menu()
+    main_menu_input = input("Select an option: ")
+    main_menu_option = int(main_menu_input)
 
-    # MACHINE CHOICE
-    machine_id_input = input("Select a machine (number - enter 0 to quit): ")
-    selected_machine_id = int(machine_id_input) - 1
+    if main_menu_option == 1:
+        # MAIN PROGRAM: MACHINE SELECTION
+        while main_menu_option == 1:
+            clear_screen()
+            sl86_header()
+            path_86box_text()
+            print("")
+            path_machine_text()
+            # MACHINE LISTING
+            machines.sort(key=str.lower)
+            print("Machines detected in directory:", machines_count, "\n")
+            for i in range(machines_count):
+                print(i + 1, "-", machines[i])
+            print("")
 
-    # EARLY EXIT
-    if int(machine_id_input) == 0:
+            # MACHINE CHOICE
+            machine_id_input = input("Select a machine (number - enter 0 to return to main menu): ")
+            selected_machine_id = int(machine_id_input) - 1
+
+            # EARLY EXIT
+            if int(machine_id_input) == 0:
+                clear_screen()
+                sl86_header()
+                path_86box_text()
+                print("")
+                path_machine_text()
+                break
+
+            # LAUNCH, CONFIGURE, RETURN, QUIT?
+            print("")
+            print("Machine selected: " + machine_id_input + " (" + machines[selected_machine_id] + ")")
+            machine_decision_input = input("[L]aunch / [C]onfigure / [M]achine list / [R]eturn to main menu: ")
+
+            if machine_decision_input == 'L' or machine_decision_input == 'l':
+                # MACHINE EXECUTION BASED ON USER CHOICE
+                print("\nMachine " + machine_id_input + " (" + machines[selected_machine_id] + ") selected for launch.\n")
+                print("Starting 86Box...")
+                subprocess.run([app_path, "-P", f"{machine_path}/{machines[selected_machine_id]}"])
+            elif machine_decision_input == 'C' or machine_decision_input == 'c':
+                # MACHINE CONFIGURATION BASED ON USER CHOICE
+                print("\nMachine " + machine_id_input + " (" + machines[selected_machine_id] + ") selected for configuration.\n")
+                print("Starting 86Box...")
+                subprocess.run([app_path, "-S", f"{machine_path}/{machines[selected_machine_id]}/86box.cfg"])
+            elif machine_decision_input == 'M' or machine_decision_input == 'M':
+                clear_screen()
+                print("")
+                path_86box_text()
+                print("")
+                path_machine_text()
+                continue
+            elif machine_decision_input == 'r' or machine_decision_input == 'r':
+                clear_screen()
+                sl86_header()
+                path_86box_text()
+                print("")
+                path_machine_text()
+                break
+            else:
+                print("Invalid input - try again.")
+            clear_screen()
+            print("")
+            path_86box_text()
+            path_machine_text()
+    elif main_menu_option == 2:
         clear_screen()
-        print("")
-        quit_text()
-        print("")
-        break
-
-    # LAUNCH, CONFIGURE, RETURN, QUIT?
-    print("")
-    print("Machine selected: " + machine_id_input + " (" + machines[selected_machine_id] + ")")
-    machine_decision_input = input("[L]aunch / [C]onfigure / [R]eturn to machine list / [Q]uit launcher? ")
-
-    if machine_decision_input == 'L' or machine_decision_input == 'l':
-        # MACHINE EXECUTION BASED ON USER CHOICE
-        print("\nMachine " + machine_id_input + " (" + machines[selected_machine_id] + ") selected for launch.\n")
-        print("Starting 86Box...")
-        subprocess.run([app_path, "-P", f"{machine_path}/{machines[selected_machine_id]}"])
-    elif machine_decision_input == 'C' or machine_decision_input == 'c':
-        # MACHINE CONFIGURATION BASED ON USER CHOICE
-        print("\nMachine " + machine_id_input + " (" + machines[selected_machine_id] + ") selected for configuration.\n")
-        print("Starting 86Box...")
-        subprocess.run([app_path, "-S", f"{machine_path}/{machines[selected_machine_id]}/86box.cfg"])
-    elif machine_decision_input == 'R' or machine_decision_input == 'r':
-        clear_screen()
-        print("")
-        path_86box_text()
-        path_machine_text()
+        sl86_header()
+        info_86box()
         continue
-    elif machine_decision_input == 'Q' or machine_decision_input == 'q':
+    elif main_menu_option == 0:
         clear_screen()
         print("")
         quit_text()
         print("")
         break
     else:
+        clear_screen()
+        sl86_header()
         print("Invalid input - try again.")
-
-    clear_screen()
-    print("")
-    path_86box_text()
-    path_machine_text()
+        print("")
